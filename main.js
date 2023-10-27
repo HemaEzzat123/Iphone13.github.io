83979af01403220145243efb78bc92418d3334ae
Ibrahim Ezzat <114920415+HemaEzzat123@users.noreply.github.com>
2023-10-27 17:15:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let photo = document.querySelector(".photo");
+let container = document.querySelector("body");
+function phones(phone) {
+  photo.src = phone;
+}
+function colors(color) {
+  container.style.background = color;
+}
